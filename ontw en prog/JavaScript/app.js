bb6c4cbf319cddeb8407a846fb74ee0e2c1b0711
EmreHBas <9003733@student.zadkine.nl>
2022-10-19 10:07:39 +0200

    alles

---
+++ ontw en prog/JavaScript/app.js
@@ -0,0 +1,9 @@
+ 
+var kleur= prompt("geef een Engelse kleur", ""); 
+
+// kleur
+ document.getElementById("titel").style.color = kleur; 
+
+//tekst aanpassen
+document.getElementById.ById("p1").innerHTML = "doei";
+
